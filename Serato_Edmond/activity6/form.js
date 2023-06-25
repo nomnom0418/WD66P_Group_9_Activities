9f8832f486ec351c2287ceeac69a982dcea56f91
Edmond Serato <alphashadow0418@gmail.com>
2023-06-25 15:26:40 +0800

    add remove function in DOM starting reg form

--- Serato_Edmond/activity6/form.js
+++ Serato_Edmond/activity6/form.js
@@ -1,38 +1,29 @@
-let defaultUserName = "admin"
-let defaultPassword = "password"
-
 let myWarning = document.getElementById("head");
-
-function logIn(remove){
+let children = myWarning.getElementsByClassName('myChildren');
+let myPrompt;
+function logIn(){
   let defaultUserName = "admin"
   let defaultPassword = "password"
   let myUserName = document.getElementById("userName").value;
   let myPassword = document.getElementById("password").value;
-  let children = myWarning.getElementsByClassName('myChildren');
-  let myPrompt;
+
 
   if (myUserName == defaultUserName && myPassword == defaultPassword){
-    while (children.length > 0) {
-      children[0].parentNode.removeChild(children[0]);
-    }
+    remove()
     myPrompt = document.createElement("div");
     myPrompt.classList = "myChildren success";
     myPrompt.textContent = "Succesfully Login";
     myWarning.appendChild(myPrompt);
   }
   else if (myUserName == "" && myPassword == ""){
-    while (children.length > 0) {
-      children[0].parentNode.removeChild(children[0]);
-    }
+    remove()
     myPrompt = document.createElement("div");
     myPrompt.classList = "myChildren warningEmpty";
     myPrompt.textContent = "please input username and password";
     myWarning.appendChild(myPrompt);
   }
   else{
-    while (children.length > 0) {
-      children[0].parentNode.removeChild(children[0]);
-    }  
+    remove()
     myPrompt = document.createElement("div");
     myPrompt.classList = "myChildren warningWrongPassword";
     myPrompt.textContent = "invalid username or password";
@@ -40,4 +31,8 @@ function logIn(remove){
   }
   
 }
-
+function remove(){
+  while (children.length > 0) {
+    children[0].parentNode.removeChild(children[0]);
+  }  
+}
