d6e3ad01f162f045a4d90cf27760b5e47aec66ea
Edmond Serato <alphashadow0418@gmail.com>
2023-06-25 15:18:14 +0800

    done login starting register form

--- Serato_Edmond/activity6/form.js
+++ Serato_Edmond/activity6/form.js
@@ -1,26 +1,43 @@
 let defaultUserName = "admin"
 let defaultPassword = "password"
 
+let myWarning = document.getElementById("head");
 
-
-function logIn(){
+function logIn(remove){
   let defaultUserName = "admin"
   let defaultPassword = "password"
   let myUserName = document.getElementById("userName").value;
   let myPassword = document.getElementById("password").value;
-  let myWarning = document.getElementById("head");
+  let children = myWarning.getElementsByClassName('myChildren');
   let myPrompt;
 
-    if (myUserName == defaultUserName && myPassword == defaultPassword){
+  if (myUserName == defaultUserName && myPassword == defaultPassword){
+    while (children.length > 0) {
+      children[0].parentNode.removeChild(children[0]);
+    }
     myPrompt = document.createElement("div");
-    myPrompt.id = "myChilden";
+    myPrompt.classList = "myChildren success";
     myPrompt.textContent = "Succesfully Login";
     myWarning.appendChild(myPrompt);
+  }
+  else if (myUserName == "" && myPassword == ""){
+    while (children.length > 0) {
+      children[0].parentNode.removeChild(children[0]);
     }
-    else{
     myPrompt = document.createElement("div");
-    myPrompt.id = "myChilden";
+    myPrompt.classList = "myChildren warningEmpty";
+    myPrompt.textContent = "please input username and password";
+    myWarning.appendChild(myPrompt);
+  }
+  else{
+    while (children.length > 0) {
+      children[0].parentNode.removeChild(children[0]);
+    }  
+    myPrompt = document.createElement("div");
+    myPrompt.classList = "myChildren warningWrongPassword";
     myPrompt.textContent = "invalid username or password";
     myWarning.appendChild(myPrompt);
-    }
+  }
+  
 }
+
